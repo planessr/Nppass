d6cdd48aff8208005e54c35d0217306426a6d417
planessr <109939648+planessr@users.noreply.github.com>
2025-09-15 00:24:27 +0800

    更新 vite.config.ts

--- vite-frontend/vite.config.ts
+++ vite-frontend/vite.config.ts
@@ -1,36 +1,31 @@
- import { defineConfig } from "vite";
- import react from "@vitejs/plugin-react";
- import path from "path";
- import legacy from '@vitejs/plugin-legacy'
-  
- export default defineConfig({
-   plugins: [
-     react(),
-     legacy({
-       targets: ['chrome>=87', 'firefox>=78', 'safari>=15', 'edge>=89'],
-       modernPolyfills: ['es.bigint', 'es.object.entries'],
-       polyfills: ['es.bigint.constructor']
-     })
-   ],
-   base: './',
-   resolve: { alias: { "@": path.resolve(__dirname, "./src") } },
-   server: { port: 3000, host: '0.0.0.0' },
-   build: {
-     outDir: 'dist',
-     sourcemap: false,
-     minify: false,
-     target: 'es2020',
-     rollupOptions: {
-       treeshake: false,
-       output: {
-         generatedCode: 'es2020',
-         preserveModules: false, // 必须关闭 
-         preserveEntrySignatures: 'strict' // 严格模式
-       }
-     }
-   },
-   esbuild: {
-     target: 'es2020',
-     supported: { 'bigint': true }
-   }
- });
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import path from "path";
+import legacy from '@vitejs/plugin-legacy'
+
+export default defineConfig({
+  plugins: [
+    react(),
+    legacy({
+      targets: ['defaults', 'not IE 11']
+    })
+  ],
+  base: './',    
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
+  server: {
+    port: 3000,
+    host: '0.0.0.0'
+  },
+  build: {
+    outDir: 'dist',
+    sourcemap: false,
+    minify: false,  
+    rollupOptions: {
+      treeshake: false,
+    }
+  }
+});
