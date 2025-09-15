e2106f2a1b40204b03f9c0402d12423e6bc93b07
planessr <109939648+planessr@users.noreply.github.com>
2025-09-15 17:13:14 +0800

    更新 vite.config.ts

--- vite-frontend/vite.config.ts
+++ vite-frontend/vite.config.ts
@@ -2,9 +2,11 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import path from "path";
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react()
+  ],
+  base: './',    
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
@@ -17,9 +19,9 @@ export default defineConfig({
   build: {
     outDir: 'dist',
     sourcemap: false,
-    minify: false,       // 先关闭压缩，排查是否压缩导致
+    minify: false,  
     rollupOptions: {
-      treeshake: false,  // 关闭 treeshake（摇树优化），排查是否优化导致
+      treeshake: false,
     }
   }
 });
