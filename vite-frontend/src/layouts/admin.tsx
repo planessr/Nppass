02121393fad14eff68958bfaa6d5d32b78669d7a
planessr <109939648+planessr@users.noreply.github.com>
2025-09-21 10:19:45 +0800

    更新 admin.tsx

--- vite-frontend/src/layouts/admin.tsx
+++ vite-frontend/src/layouts/admin.tsx
@@ -309,7 +309,7 @@ export default function AdminLayout({
         <div className="px-4 py-2 pb-4 mt-auto flex-shrink-0">
           <div className="text-center">
             <p className="text-xs text-gray-400 dark:text-gray-500">
-              Powered by flux-panel
+              Powered by Nppass
             </p>
           </div>
         </div>
