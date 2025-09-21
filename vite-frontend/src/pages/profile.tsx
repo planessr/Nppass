ba764c1774aeaa305d1914de0a0965270cf9c8c2
planessr <109939648+planessr@users.noreply.github.com>
2025-09-21 10:41:17 +0800

    更新 profile.tsx

--- vite-frontend/src/pages/profile.tsx
+++ vite-frontend/src/pages/profile.tsx
@@ -240,7 +240,7 @@ export default function ProfilePage() {
 
         <div className="fixed inset-x-0 bottom-20 text-center py-4">
                <p className="text-xs text-gray-400 dark:text-gray-500">
-                 Powered by flux-panel
+                 Powered by Nppass
                </p>
                <p className="text-xs text-gray-400 dark:text-gray-500 mt-1">
                  v{ isWebViewFunc() ? siteConfig.app_version : siteConfig.version}
